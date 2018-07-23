716ae57331c86c324e5c4d0017eb9517c180c6fb
MrAnsheff <mransheff86@gmail.com>
2018-07-24 03:39:49 +0400

    My First StatefulWidget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,19 +3,28 @@ import 'package:flutter/material.dart';
 
 // Create a class that will be out custom widget
 // This widget must extend "StatelessWidget" base class
+class App extends StatefulWidget{
+  createState() {
+      // TODO: implement createState
+      return AppState();
+  }
+}
 
-class App extends StatelessWidget{
+class AppState extends State<App>{
 
 // Must define the 'build' method that returns
 // the widgets, that *this* widget will show
-
+var counter = 0;
 Widget build(context){
   return MaterialApp(
     home: Scaffold(
+      body: Text('$counter'),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: (){
-          print('Hi there!');
+          setState(() {
+                      counter += 1;
+                    });
         },
       ),
       appBar: AppBar(
