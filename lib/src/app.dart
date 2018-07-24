7b74a1b9c6e372be2676752e991408351bd4fcb3
MrAnsheff <mransheff86@gmail.com>
2018-07-25 02:23:05 +0400

    Complete Image Parse App

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,9 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'dart:convert';
+import 'widgets/image_list.dart';
 
 // Create a class that will be out custom widget
 // This widget must extend "StatelessWidget" base class
@@ -15,17 +19,25 @@ class AppState extends State<App>{
 // Must define the 'build' method that returns
 // the widgets, that *this* widget will show
 var counter = 0;
+List<ImageModel> images = [];
+
+void fetchImage() async{
+  counter++;
+  var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+  var imageModel = new ImageModel.fromJson(json.decode(response.body));
+  setState(() {
+      images.add(imageModel);
+    });
+
+}
+
 Widget build(context){
   return MaterialApp(
     home: Scaffold(
-      body: Text('$counter'),
+      body: ImageList(images),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: (){
-          setState(() {
-                      counter += 1;
-                    });
-        },
+        onPressed: fetchImage,
       ),
       appBar: AppBar(
         title: Text('Давайте посмотрим картинки!')
